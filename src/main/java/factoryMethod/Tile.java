1a213a48b00fc9ccf987a15d4e96e39be999ee77
Jasper Kaira <112175831+japuter@users.noreply.github.com>
2025-08-26 11:44:42 +0300

    Add project files

---
+++ src/main/java/factoryMethod/Tile.java
@@ -0,0 +1,7 @@
+package factoryMethod;
+
+public interface Tile {
+    char getCharacter(); // // Palauttaa laattaa edustavan merkin
+    String getType();    // Palauttaa laatan tyypin merkkijonona 
+
+}
